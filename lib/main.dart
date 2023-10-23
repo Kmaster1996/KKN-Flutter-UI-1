22e99c090beb939d3a20d96306464104e0c3c3fd
KKNMASTER <k.sakmongkoljit@gmail.com>
2023-10-23 23:02:09 +0700

    KKN Flutter UI 1 V1

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Docker());
+}
+
+class Docker extends StatelessWidget {
+  const Docker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Docker'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                //Advertiser
+                Text('Hackathon time! Join us for the Docker AI/ML Hackathon now through November 7th. Sign up now')
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                //Menu and option
+                Column(
+                  children: <Widget>[
+                    Text('Menu')
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text('Sign in')
+                  ],
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                //Word
+                Text('Pricing & Subscriptions'),
+                Text('Choose one that right for you.')
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                //Price
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
